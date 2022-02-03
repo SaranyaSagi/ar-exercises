cdf0032848ba31f0a284590987ce7ea981298f4a
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-03 00:31:01 -0500

    relate two tables and add emplyees to store 1 and store 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Andrew", last_name: "Dale", hourly_rate: 50)
+@store1.employees.create(first_name: "Emma", last_name: "Stan", hourly_rate: 100)
+
+@store2.employees.create(first_name: "Oscar", last_name: "Peterson", hourly_rate: 90)
+@store2.employees.create(first_name: "Maddy", last_name: "Bowen", hourly_rate: 81)
+@store2.employees.create(first_name: "Tim", last_name: "Clark", hourly_rate: 55)
